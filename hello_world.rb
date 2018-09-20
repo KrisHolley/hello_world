db4275b58687e446315adfbd1593725b5bceb2b7
KrisHolley <krisholley_09@hotmail.com>
2018-09-20 14:59:39 -0400

    odd numbers changed to odd

--- hello_world.rb
+++ hello_world.rb
@@ -1,3 +1,12 @@
-1.upto(100) do |x|
-	puts x
+#  1.upto(100) do |x|
+# 	puts x 
+# end
+count = 1 
+until count == 101
+	if count.odd? == true
+		puts"odd"
+	else
+	puts count 
+	end
+	count = count+1
 end
