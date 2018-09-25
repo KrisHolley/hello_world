abf0c6afd51eb89fb777c0809a38135ff546df66
KrisHolley <krisholley_09@hotmail.com>
2018-09-25 17:05:43 -0400

    mmkata

---
+++ mined_minds_kata/mined_minds_kata.rb
@@ -0,0 +1,16 @@
+# (1..100).each do |x|
+ 
+#   output = ""
+#   output += "Fizz" if x % 3 == 0
+#   output += "Buzz" if x % 5 == 0
+#   output = x if output.empty?
+ 
+#   puts output
+# end
+def mmkata(num)
+	if num == 3
+		"mined"
+	else
+	num
+	end
+end
---
+++ mined_minds_kata/test_mined_minds_kata.rb
@@ -0,0 +1,19 @@
+require "minitest/autorun"
+require_relative "mined_minds_kata.rb"
+class My_test < Minitest::Test
+	def test_number_one
+		assert_equal(1,1)
+	end
+	def test_number_one_return_one
+		assert_equal(1,mmkata(1))
+	end
+	def test_number_two_return_two
+		assert_equal(2,mmkata(2))
+	end
+	def test_number_17
+		assert_equal(17,mmkata(17))
+	end
+	def test_number_3
+		assert_equal("mined",mmkata(3))
+	end
+end
